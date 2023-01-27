97f3b14be7d78546bc8e0f6511a2072e514313f5
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2023-01-27 11:56:52 -0300

    Initial Commit

---
+++ SwUi_Gesture/SwUi_GestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwUi_GestureApp.swift
+//  SwUi_Gesture
+//
+//  Created by Marcelo Sampaio on 27/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SwUi_GestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
